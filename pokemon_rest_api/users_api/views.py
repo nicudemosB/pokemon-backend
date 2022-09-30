85ac8d3dabf730b34cf3f1067adf08d5498b9018
Billy <billyn0213@gmail.com>
2022-09-30 10:57:37 -0400

    restructured files

---
+++ pokemon_rest_api/users_api/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+from .serializers import UserSerializer
+from .models import User
+# Create your views here.
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all().order_by('id')
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all().order_by('id')
+    serializer_class = UserSerializer
